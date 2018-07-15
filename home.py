10df2c920ce627084575838862f7627daee97695
Jacob Garcia <garciaj4@oregonstate.edu>
2018-07-15 17:42:35 -0500

    post and get methods for boat

--- home.py
+++ home.py
@@ -2,20 +2,45 @@ from google.appengine.ext import ndb
 import webapp2
 import json
 
-# [START main_page]
+class Boat(ndb.Model):
+    name = ndb.StringProperty()
+    type = ndb.StringProperty()
+    length = ndb.IntegerProperty()
+    at_sea = ndb.BooleanProperty()
+
+class BoatHandler(webapp2.RequestHandler):
+    def post(self):
+        boat_data = json.loads(self.request.body) 
+        new_boat = Boat(name=boat_data['name'], type=boat_data['type'], length=boat_data['length'], at_sea=boat_data['at_sea'])
+        new_boat.put()
+        boat_dict = new_boat.to_dict()
+        boat_dict['self'] = '/boat/' + new_boat.key.urlsafe()
+        self.response.write(json.dumps(boat_dict))
+
+    def get(self, id=None):
+        if id:
+            b = ndb.Key(urlsafe=id).get()
+            b_d = b.to_dict()
+            b_d['self'] = "/boat/" + id
+            self.response.write(json.dumps(b.to_dict()))
+        else:
+            all_boats = [] 
+            boat_query = Boat.query()
+            boats = boat_query.fetch()
+            for each_boat in boats:
+                newBoat_d = each_boat.to_dict()
+                all_boats.append(newBoat_d)
+                #boats_d = all_boats.to_dict()
+                self.response.write(json.dumps(all_boats))
+
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        
-
-# [END main_page]
-
-allowed_methods = webapp2.WSGIApplication.allowed_methods
-new_allowed_methods = allowed_methods.union(('PATCH',))
-webapp2.WSGIApplication.allowed_methods = new_allowed_methods
+        self.response.write('hello')
 
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/boat', BoatHandler),
+    ('/boat/(.*)', BoatHandler)
 ], debug=True)
 # [END app]
